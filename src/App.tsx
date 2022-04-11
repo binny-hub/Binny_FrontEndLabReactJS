63eca9ad59c1d8fda4d2951b79efa045a9eb0c7f
binny-hub <78693918+binny-hub@users.noreply.github.com>
2022-04-11 22:58:52 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+ import ExpenseTracker from "./components/ExpenseTracker";
+
+ import 'bootstrap/dist/css/bootstrap.min.css';
+ 
+ function App() {
+     return (
+         <div>
+             <ExpenseTracker />
+         </div>
+     );
+ }
+ 
+ export default App;
